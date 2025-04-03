ad459afa4235d38614050210427163ac8bea06d3
unknown <shanebyrne0963@gmail.com>
2025-04-03 20:53:15 +0100

    Update sale button to display sale toggle text

--- assets/js/productdetail.js
+++ assets/js/productdetail.js
@@ -1,10 +1,22 @@
-if (window.location.href.includes("/product/")) {
+// Creates a sale button under the price display
+function saleButtonInit() {
     const priceEl = document.querySelector("p.price");
     priceEl.style.marginBottom = "0";
+    const isSale = priceEl.querySelectorAll(".woocommerce-Price-amount").length > 1;
 
     const priceButton = document.createElement("button");
-    priceButton.className = "br-ext";
-    priceButton.innerText = "Click Me!";
+    priceButton.className = `br-ext`;
+
+    if (isSale) {
+        priceButton.innerText = "End Sale";
+    }
+    else {
+        priceButton.innerText = "Start Sale";
+    }
 
     priceEl.after(priceButton);
+}
+
+if (window.location.href.includes("/product/")) {
+    saleButtonInit();
 }
