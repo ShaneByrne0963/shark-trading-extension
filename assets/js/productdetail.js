70b1e1a89bfa7608156b1e5f1c06a922960e45ee
unknown <shanebyrne0963@gmail.com>
2025-04-04 10:06:22 +0100

    Update sale button to take admin to edit product page

--- assets/js/productdetail.js
+++ assets/js/productdetail.js
@@ -1,3 +1,9 @@
+// Takes the admin to the edit product page
+function editProduct() {
+    const productId = document.getElementById("product-id-for-enquiry").value;
+    window.location = `/wp-admin/post.php?post=${productId}&action=edit`;
+}
+
 // Creates a sale button under the price display
 function saleButtonInit() {
     const priceEl = document.querySelector("p.price");
@@ -13,7 +19,7 @@ function saleButtonInit() {
     else {
         priceButton.innerText = "Start Sale";
     }
-
+    priceButton.onclick = editProduct;
     priceEl.after(priceButton);
 }
 
