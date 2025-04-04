d77cae55242ea9beb30a6a34119ac5df6d082922
unknown <shanebyrne0963@gmail.com>
2025-04-04 10:42:21 +0100

    Add sale form on start sale button click

---
+++ assets/js/editproduct.js
@@ -0,0 +1,3 @@
+function returnToProduct() {
+  window.location = document.querySelector("#sample-permalink>a").href;
+}
--- assets/js/productdetail.js
+++ assets/js/productdetail.js
@@ -4,13 +4,24 @@ function editProduct() {
     window.location = `/wp-admin/post.php?post=${productId}&action=edit`;
 }
 
+
+function initSale() {
+    let saleForm = document.getElementById("br-ext-sale-form");
+    let saleButton = document.getElementById("br-ext-start-sale");
+
+    saleButton.style.display = "none";
+    saleForm.style.display = "flex";
+}
+
+
 // Creates a sale button under the price display
 function saleButtonInit() {
     const priceEl = document.querySelector("p.price");
     priceEl.style.marginBottom = "0";
     const isSale = priceEl.querySelectorAll(".woocommerce-Price-amount").length > 1;
 
-    const priceButton = document.createElement("button");
+    let priceButton = document.createElement("button");
+    priceButton.id = `br-ext-start-sale`;
     priceButton.className = `br-ext`;
 
     if (isSale) {
@@ -18,8 +29,37 @@ function saleButtonInit() {
     }
     else {
         priceButton.innerText = "Start Sale";
+        priceButton.onclick = initSale;
+
+        // Create the sale form so the admin can set the sale price on the product detail page
+        // We don't use a form element because we are not posting data in a normal way
+        let saleForm = document.createElement("div");
+        saleForm.id = "br-ext-sale-form";
+        saleForm.className = "br-ext";
+        saleForm.style.display = "none";
+
+        // Create the sale input
+        let saleInput = document.createElement("input");
+        saleInput.id = "br-ext-sale-input";
+        saleInput.className = "br-ext";
+        saleForm.appendChild(saleInput);
+
+        // Create the submit button
+        let saleSubmit = document.createElement("button");
+        saleSubmit.id = "br-ext-sale-submit";
+        saleSubmit.className = "br-ext";
+        saleSubmit.innerText = "Submit";
+        saleForm.appendChild(saleSubmit);
+
+        // Create the cancel button
+        let saleCancel = document.createElement("button");
+        saleCancel.id = "br-ext-sale-cancel";
+        saleCancel.className = "br-ext cancel";
+        saleCancel.innerText = "Cancel";
+        saleForm.appendChild(saleCancel);
+
+        priceEl.after(saleForm);
     }
-    priceButton.onclick = editProduct;
     priceEl.after(priceButton);
 }
 
